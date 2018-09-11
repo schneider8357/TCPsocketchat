444833e1e4484d5e96a1f24d31fffbcebc8e8e8d
schneider8357 <40526466+schneider8357@users.noreply.github.com>
2018-09-11 17:22:26 -0300

    client v5

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ import _thread
 def receber(cliente):
 	while True:
 		msg = cliente.recv(1024).decode('utf-8')
-		print('\n{0}\nDigite sua mensagem:'.format(msg), end='')
+		print('\n{0}\nDigite a mensagem: '.format(msg), end='')
 	cliente.close()
 
 def enviar(cliente):
@@ -18,7 +18,15 @@ def enviar(cliente):
 		if msg == '/exit': break
 	cliente.close()
 
-
+def setLogin():
+	while True:
+		msg = cliente.recv(1024).decode('utf-8')
+		if msg == '/loginok': break
+		print(msg, end='')
+		login = str(input())
+		if not login: login = '\x00'
+		cliente.send(login.encode('utf-8'))
+	return login
 
 os.system('clear')
 
@@ -26,18 +34,10 @@ HOST = '127.0.0.1'
 PORT = str(input('Digite o número de porta em que o cliente TCP irá rodar (default = 50000): '))
 if (not PORT.isdigit()) or (int(PORT) > 65535) or (int(PORT) < 1024): PORT = '50000'
 
+
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 cliente.connect((HOST, int(PORT)))
-
-while True:
-	msg = cliente.recv(1024).decode('utf-8')
-	if msg == '/loginok': break
-	print(msg, end='')
-	login = str(input())
-	if not login: login = '\x00'
-	cliente.send(login.encode('utf-8'))
-
-print('Para sair, use o comando \'/exit\'\n')
+login = setLogin()
+print('Bem vindo, %s! Para sair, use o comando \'/exit\'\n'%login)
 _thread.start_new_thread(receber, tuple([cliente]))
 enviar(cliente)
