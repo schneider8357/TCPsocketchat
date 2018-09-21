4f88a5b5ac3b41fbca0a22f2393c235514cd8750
schneider8357 <40526466+schneider8357@users.noreply.github.com>
2018-09-21 18:04:49 -0300

    Implementação de comandos e correções de bug

--- chat-suapi/client.py
+++ chat-suapi/client.py
@@ -5,48 +5,83 @@ import socket
 import _thread
 import getpass
 
+
+
+def ajuda():
+	print('-----> Para sair, use o comando \'/exit\'')
+	print('-----> Para limpar a tela, use o comando \'/clear\'')
+	print('-----> Para exibir a lista de clientes conectados, use o comando \'/clients\'')
+	print('-----> Para enviar mensagens privadas, use o comando \'/msg\' (beta)')
+	print('-----> Para pedir ajuda, use o comando \'/help\'')
+
+def comando(msg):
+	if msg == '/clear': os.system('clear')
+	elif msg == '/help': ajuda()
+	elif msg == '/clients' or msg.split()[0] == '/msg': cliente.send(msg.encode('utf-8'))
+	else: print('Comando \'%s\' não reconhecido.'%msg.split()[0])
+
 def receber(cliente):
 	while True:
 		msg = cliente.recv(1024).decode('utf-8')
+		if not msg: break
 		print('\n{0}\nDigite a mensagem: '.format(msg), end='')
+	print('\nServidor inalcançável.\n')
+	os._exit(0)
 	cliente.close()
-	_thread.exit()
 
-def enviar(cliente):
+def enviar():
 	while True:
 		msg = input('Digite a mensagem: ')
-		msgf = msg.encode('utf-8')
-		if msg == "/clear": os.system('clear')
-		else: cliente.send(msgf)
-		if msg == '/exit': break
+		if not msg: pass
+		elif msg == '/exit':
+			cliente.send(msg.encode('utf-8'))
+			break
+		elif msg[0] == '/': comando(msg)
+		else: cliente.send(msg.encode('utf-8'))
 	cliente.close()
+	print('\nConexão encerrada.\n')
+	os._exit(0)
 
 def setLogin():
 	while True:
 		msg = cliente.recv(1024).decode('utf-8')
-		if msg.split()[0] == '/loginok': break
+		if msg == '/loginok': break
 		print(msg, end='')
 		login = str(input())
 		if not login: login = '\x00'
 		cliente.send(login.encode('utf-8'))
 		msg = cliente.recv(1024).decode('utf-8')
-		senha = getpass.getpass(msg)
-		
-		if not senha: senha = '\x00'
-		cliente.send(senha.encode('utf-8'))
-	login = msg[9:]
+		if not msg == '/skip':							#	Caso o servidor envie '/skip', o cliente não precisa
+			senha = getpass.getpass(msg)				#	de senha para acessar o chat.
+			if not senha: senha = '\x00'				#	Isso permite que menos modificações sejam feitas no cliente,
+			cliente.send(senha.encode('utf-8'))			#	para melhor compatibilidade.
+	login = cliente.recv(1024).decode('utf-8')
 	return login
 
+#MAIN
 os.system('clear')
 
 HOST = '127.0.0.1'
 PORT = str(input('Digite o número de porta em que o cliente TCP irá rodar (default = 50000): '))
 if (not PORT.isdigit()) or (int(PORT) > 65535) or (int(PORT) < 1024): PORT = '50000'
 
-
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((HOST, int(PORT)))
-login = setLogin()
-print('\nBem vindo, %s!\n----->Para sair, use o comando \'/exit\'\n----->Para limpar a tela, use o comando \'/clear\'\n'%login)
+
+try: cliente.connect((HOST, int(PORT)))
+except:
+	print('\nServidor não encontrado.\n')
+	os._exit(0)
+
+try: login = setLogin()
+except:
+	cliente.close()
+	print('\nNão foi possível efetuar o login.\n')
+	os._exit(0)
+
+print('\nBem vindo, %s!'%login)
+ajuda()
 _thread.start_new_thread(receber, tuple([cliente]))
-enviar(cliente)
+
+try: enviar()
+except: cliente.close()
+
--- chat-suapi/server.py
+++ chat-suapi/server.py
@@ -8,125 +8,197 @@ import json
 import requests
 
 
-urls = { "token":"https://suap.ifrn.edu.br/api/v2/autenticacao/token/",
-		 "dados":"https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/"}
-logins = {}
-conexoes = {}
-exitall = False
+urls = { 'token':'https://suap.ifrn.edu.br/api/v2/autenticacao/token/',
+		 'dados':'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/'}
+logins = {}		# { cliente : login }
+conexoes = {}	# { cliente : conexao (socket) e hora de início da conexão }
+mensagens = []
 
 
 #FUNCOES
-def envioBroadcast(msg, remetente):
+
+def logAdd(msg): # Exibe na tela e adiciona ao log uma mensagem.
+	mensagens.append('%s'%msg)
+	print(msg)
+	
+def logRec(): # Salva o log do chat em um arquivo.
+	print('\nFazendo backup das mensagens...\n')
+	now = datetime.now()
+	now = '{0:0=2d}{1:0=2d}{2:0=2d}_{3:0=2d}{4:0=2d}{5}'.format(now.hour, now.minute, now.second, now.day, now.month, now.year)
+	with open('%s.txt' %(now), 'w') as arq:
+		for msg in mensagens:
+			arq.write('%s\n'%msg)
+		print('\nBackup realizado com sucesso! Log: %s.txt\n'%(now))
+
+def envioBroadcast(msg, remetente): # Envia uma mensagem a todos os clientes exceto àquele que enviou (remetente)
 	for cliente in conexoes:
 		con = conexoes[cliente][0]
 		if cliente == remetente: continue
-		msgf = msg.encode('utf-8')
-		con.send(msgf)
+		try:
+			con.send(msg.encode('utf-8'))
+		except:
+			continue
 	_thread.exit()
 
-def agora():
+def envioPrivado(con,cliente,msg):
+	if len(msg.split()) >= 3:
+		a = msg.split()
+		a.pop(0)
+		a.pop(len(a) - 1)
+		loginDestino = ''
+		for i in a: loginDestino += i + ' '
+		loginDestino = loginDestino[:-1]
+		loginOrigem = logins[cliente]
+		if loginOrigem == loginDestino:
+			con.send('Você não pode enviar uma mensagem privada para si.'.encode('utf-8'))
+		elif loginDestino in logins.values():
+			clienteOrigem = (list(logins.keys())[list(logins.values()).index(loginOrigem)])
+			clienteDestino = (list(logins.keys())[list(logins.values()).index(loginDestino)])
+			mensagem = msg.split()[len(msg.split()) -1]
+			logAdd('{0} {1} {2} pm {3} {4}: {5}'.format(agora(), loginOrigem, clienteOrigem, loginDestino, clienteDestino, mensagem))
+			msg = '{0} {1} diz (privado): {2}'.format(agora(), logins[cliente], mensagem)
+			conexoes[clienteDestino][0].send(msg.encode('utf-8'))
+		else:
+			m = 'O cliente de login %s não está online.\nO uso do comando \'/msg\' é: \'/msg login_destino sua_mensagem\''%loginDestino
+			con.send(m.encode('utf-8'))
+	else:
+		con.send('Argumentos insuficientes para o comando \'/msg\'\nO uso do comando é: \'/msg login_destino sua_mensagem\''.encode('utf-8'))
+
+def agora(): # Retorna a data e hora atuais.
 	now = datetime.now()
-	return '{0}:{1} {2}/{3}/{4}'.format(now.hour, now.minute, now.day, now.month, now.year)
+	return '{0:0=2d}:{1:0=2d} {2:0=2d}/{3:0=2d}/{4}'.format(now.hour, now.minute, now.day, now.month, now.year)
 
-def mostrarConexoes():
-	print('{0} <servidor> Número de clientes conectados: {1}'.format(agora(),len(conexoes)))
-	if len(conexoes) > 0:
-		for cliente in logins:
-			print('{0} <servidor> Login: {1} Cliente: {2} Conectado desde: {3}'.format(agora(),logins[cliente],cliente,conexoes[cliente][1]))
+def mostrarConexoes(i): # Exibe todos os clientes conectados.
+	if i:
+		msg = '{0} <servidor> Número de clientes conectados: {1}'.format(agora(), len(conexoes))
+		if len(conexoes):
+			for cliente in logins:
+				msg = msg + '\n{0} <servidor> Login: {1} Conectado desde: {2}'.format(agora(), logins[cliente], conexoes[cliente][1])
+		return(msg)
+	else: logAdd('{0} <servidor> Número de clientes conectados: {1}'.format(agora(),len(conexoes)))
+# Caso queira que a cada conexão feita ou desfeita sejam exibidos todos os clientes  ativos, descomente as linhas abaixo.
 
-def getToken(autenticacao):
+#	if len(conexoes):
+#		for cliente in logins:
+#			logAdd('{0} <servidor> Login: {1} Cliente: {2} Conectado desde: {3}'.format(agora(), logins[cliente], cliente, conexoes[cliente][1]))
+
+def getToken(autenticacao): # Retorna o token para acesso ao SUAP.
 	response = requests.post(urls['token'], data=autenticacao)
 	if response.status_code == 200:
 		return json.loads(response.content.decode('utf-8'))['token']
 	return None
 
-def getInformacoes(cabecalho):
+def getInformacoes(cabecalho): # Retorna os dados utilizando o token.
 	response = requests.get(urls['dados'], headers=cabecalho)
 	if response.status_code == 200:
 		return response.content.decode('utf-8')
 	return None
 
-def autentica(login,senha):
-	autenticacao = { "username": login, "password": senha }
+def autentica(login,senha): # Define o login do cliente de acordo com o nome no SUAP.
+	autenticacao = { 'username': login, 'password': senha }
 	cabecalho = {'Authorization': 'JWT {0}'.format(getToken(autenticacao))}
 	informacoes = json.loads(getInformacoes(cabecalho))
 	login = informacoes['nome_usual']
 	if login in logins.values():
 		return 'O usuário já está logado! Tente novamente!\nLogin: '
 	else:
-		return '/loginok %s'%login
+		return '/loginok', login # '/loginok seu_login_aqui' é a mensagem que confirma que o login foi efetuado com sucesso. 
 
-def setLogin(con, cliente):
-	msg = 'Matrícula: '
-	msgf = msg.encode('utf-8')
-	con.send(msgf)
+def setLogin(con, cliente):						# Conversa com a aplicação cliente para autenticá-lo.
+	msg = 'Matrícula: '							# O servidor define as mensagens que aparecerão para o cliente na hora do login.
+	con.send(msg.encode('utf-8'))				# Isso permite que poucas alterações sejam necessárias na aplicação cliente.
 	login = con.recv(1024).decode('utf-8')
 	msg = 'Senha: '
-	msgf = msg.encode('utf-8')
-	con.send(msgf)
+	con.send(msg.encode('utf-8'))
 	senha = con.recv(1024).decode('utf-8')
 	while True:
-		if login == '\x00':
-			msg = 'O login não pode ficar em branco! Tente novamente!\nLogin: '
+		if login == '\x00' or senha == '\x00':
+			msg = 'A matrícula e senha não podem ficar em branco! Tente novamente!\nMatrícula: '
 		else:
 			try:
-				msg = autentica(login,senha)
+				msg, login = autentica(login,senha)
 			except:
-				msg = 'Usuário ou senha incorretos.\nLogin: '
-		msgf = msg.encode('utf-8')
-		con.send(msgf)
-		if msg.split()[0] == '/loginok': break
+				msg = 'Usuário ou senha incorretos.\nMatrícula: '
+		con.send(msg.encode('utf-8'))
+		if msg == '/loginok': break
 		login = con.recv(1024).decode('utf-8')
 		msg = 'Senha: '
-		msgf = msg.encode('utf-8')
-		con.send(msgf)
+		con.send(msg.encode('utf-8'))
 		senha = con.recv(1024).decode('utf-8')
-	login = msg[9:]
 	logins[cliente] = login
-	now = datetime.now()
+	con.send(login.encode('utf-8'))
 	conexoes[cliente] = tuple([con,agora()])
 
 def inicioConexao(con, cliente):
-	setLogin(con, cliente)
-	msg = '{0} <servidor> {1} entrou.'.format(agora(),logins[cliente])
-	_thread.start_new_thread(envioBroadcast, tuple([msg, cliente]))
-	print('{0} <servidor> {1} {2} entrou.'.format(agora(),logins[cliente],cliente))
-	mostrarConexoes()
-	while True:
-		msg = con.recv(1024).decode('utf-8')
-		if msg == '/exit': break
-		print('{0} {1} {2}: {3}'.format(agora(),logins[cliente],cliente,msg))
-		msg = '{0} {1} diz: {2}'.format(agora(),logins[cliente],msg)
+	try:
+		setLogin(con, cliente)
+		msg = '{0} <servidor> {1} entrou.'.format(agora(), logins[cliente]) 
+		logAdd('{0} <servidor> {1} {2} entrou.'.format(agora(), logins[cliente], cliente))
+		# A mensagem enviada é diferente da que aparece no servidor, para não enviar aos clientes os endereços IP dos outros clientes.
 		_thread.start_new_thread(envioBroadcast, tuple([msg, cliente]))
-	m = '{0} <servidor> {1} saiu.'.format(agora(),logins[cliente],cliente)
-	print('{0} <servidor> {1} {2} saiu.'.format(agora(),logins[cliente],cliente))
-	del logins[cliente]
-	del conexoes[cliente]
-	mostrarConexoes()
-	_thread.start_new_thread(envioBroadcast, tuple([m, cliente]))
-	con.close()
+		mostrarConexoes(0)
+		conexao(con,cliente)
+		con.close()
+	except:
+		logAdd('{0} <servidor> Não foi possível conectar com {1}. Finalizando thread.'.format(agora(),cliente))
+		con.close()
 	_thread.exit()
 
+def conexao(con,cliente):
+	while True:
+		msg = con.recv(1024).decode('utf-8')
+		if not msg or msg == '/exit': break
+		elif msg.split()[0] == '/msg': # Executa o comando /msg (envia mensagens privadas entre clientes).
+			envioPrivado(con, cliente, msg)
+		elif msg == '/clients': # Executa o comando /clients (envia para o cliente uma lista com os clientes ativos).
+			con.send(mostrarConexoes(1).encode('utf-8'))
+		else:
+			logAdd('{0} {1} {2}: {3}'.format(agora(), logins[cliente], cliente, msg))
+			msg = '{0} {1} diz: {2}'.format(agora(), logins[cliente], msg)
+			_thread.start_new_thread(envioBroadcast, tuple([msg, cliente]))
+	fimConexao(con, cliente)
+
+def fimConexao(con, cliente):
+	m = '{0} <servidor> {1} saiu.'.format(agora(), logins[cliente], cliente)
+	logAdd('{0} <servidor> {1} {2} saiu.'.format(agora(),logins[cliente], cliente))
+	del logins[cliente]
+	del conexoes[cliente]
+	mostrarConexoes(0)
+	_thread.start_new_thread(envioBroadcast, tuple([m, cliente]))
+
+
 #MAIN
+
 os.system('clear')
-now = datetime.now()
 HOST = '127.0.0.1'
 PORT = str(input('Digite o número de porta em que o servidor TCP irá rodar (default = 50000): '))
 if (not PORT.isdigit()) or (int(PORT) > 65535) or (int(PORT) < 1024): PORT = '50000'
 
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = (HOST, int(PORT))
-server.bind(addr)
-server.listen(1)
-print('\nServidor TCP-THREAD iniciado no IP', HOST, 'na porta', PORT)
 
+
+try:
+	server.bind(addr)
+	server.listen(1)
+except:
+	print('\nNão foi possível iniciar o servidor.\n')
+	os._exit(0)
+
+m = 'Servidor TCP-THREAD iniciado no IP %s na porta %s às %s'%(HOST,PORT,agora())
+mensagens.append(m)
+print('\n%s'%m)
 
 while True:
 	try:
 		con, cliente = server.accept()
 	except KeyboardInterrupt:
 		break
-	print('{0} <servidor> nova thread iniciada para o cliente {1}'.format(agora(),cliente))
+	logAdd('{0} <servidor> Nova thread iniciada para o cliente {1}'.format(agora(),cliente))
 	_thread.start_new_thread(inicioConexao, tuple([con, cliente]))
-print("\nEncerrando servidor...\n")
+m = 'Encerrando servidor às %s...'%agora()
+mensagens.append(m)
+logRec()
+print('\n%s'%m)
 server.close()
