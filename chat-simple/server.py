3bf4691aacde037cf23f41f67452bf09a33ed57c
Lucas Schneider <schneider8357@hotmail.com>
2018-11-28 17:57:58 -0300

    Update server.py

--- chat-simple/server.py
+++ chat-simple/server.py
@@ -1,59 +1,3 @@
-#!/usr/bin/python3
-
-import socket
-import _thread
-import os
-
-HOST = '10.20.2.5'      # Endereco IP do Servidor
-PORT = 5000             # Porta que o Servidor está
-
-#-------------------------------------------------------------------------
-# Função chamada quando uma nova thread for iniciada
-def conectado(con, cliente):
-    print('\nCliente conectado:', cliente)
-
-    while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print('O Cliente {0} disse: {1}'.format(cliente,msg.decode('utf-8')))
-
-    print('\nFinalizando conexao do cliente', cliente)
-    con.close()
-    _thread.exit()
-
-
-#-------------------------------------------------------------------------
-# Bloco principal
-os.system('clear')
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-
-tcp.bind(orig)
-
-tcp.listen(1)
-print('\nServidor TCP-THREAD iniciado no IP', HOST, 'na porta', PORT)
-
-while True:
-    con, cliente = tcp.accept()
-    print('\nNova thread iniciada para essa conexão')
-
-    # Abrindo uma thread para a conexão
-    _thread.start_new_thread(conectado, tuple([con, cliente]))
-
-tcp.close()
-
-
-
------------------------------------------------------------------------------------
------------------------------------------------------------------------------------
-
------------------------------------------------------------------------------------
------------------------------------------------------------------------------------
-
------------------------------------------------------------------------------------
------------------------------------------------------------------------------------
-
 #!/usr/bin/env python3
 
 import os
@@ -62,20 +6,22 @@ import _thread
 from datetime import datetime
 import json
 import requests
+import time
 
 
 HOST = '127.0.0.1'
 urls = { 'token':'https://suap.ifrn.edu.br/api/v2/autenticacao/token/', 'dados':'https://suap.ifrn.edu.br/api/v2/minhas-informacoes/meus-dados/'}
-logins = {}	# { cliente : login }
-conexoes = {}	# { cliente : conexao (socket) }
-horaInicio = {}	# { cliente : hora de início da conexão }
+logins = {} # { cliente : login }
+conexoes = {} # { cliente : conexao (socket) }
+horaInicio = {} # { cliente : hora de início da conexão }
 mensagens = []
-
+msg = ''
 servername = '<server>'
 
 #FUNCOES
 
-def logAdd(msg): # Exibe na tela e adiciona ao log uma mensagem.
+def logAdd(dataehora, remetente, msg): # Exibe na tela e adiciona ao log uma mensagem.
+	msg = '{0} {1}: {2}'.format(dataehora, remetente, msg)
 	mensagens.append('%s'%msg)
 	print(msg)
 	
@@ -89,7 +35,8 @@ def logRec(): # Salva o log do chat em um arquivo.
 		print('\nBackup realizado com sucesso! Log: %s\n'%filename)
 
 def envioBroadcast(dataehora, remetente, msg): # Envia uma mensagem a todos os clientes exceto àquele que enviou (remetente)
-	logAdd(dataehora,remetente,msg)	
+	logAdd(dataehora,remetente,msg)
+	msg = '{0} {1}: {2}'.format(dataehora, remetente, msg)
 	for cliente in conexoes:
 		con = conexoes[cliente]
 		if cliente == remetente: continue
@@ -99,67 +46,45 @@ def envioBroadcast(dataehora, remetente, msg): # Envia uma mensagem a todos os c
 			continue
 	_thread.exit()
 
-def envioPrivado(con,cliente,msg):
-	if len(msg.split()) >= 3:
-		loginDestino = msg.split()[1]
-		loginOrigem = logins[cliente]
-		if loginOrigem == loginDestino:
-			con.send('Você não pode enviar uma mensagem privada para si.'.encode('utf-8'))
-		elif loginDestino in logins.values(): # Comente essa linha para permitir que um cliente mande uma mensagem privada para si.
-			clienteOrigem = (list(logins.keys())[list(logins.values()).index(loginOrigem)])
-			clienteDestino = (list(logins.keys())[list(logins.values()).index(loginDestino)])
-			a = msg.split()
-			a.pop(0)
-			a.pop(0)
-			mensagem = ''
-			for i in a: mensagem += i + ' '
-			mensagem = mensagem[:-1]
-			logAdd('{0} {1} {2} pm {3} {4}: {5}'.format(agora(), loginOrigem, clienteOrigem, loginDestino, clienteDestino, mensagem))
-			msg = '{0} {1} diz (privado): {2}'.format(agora(), logins[cliente], mensagem)
-			conexoes[clienteDestino].send(msg.encode('utf-8'))
-		else:
-			m = 'O cliente de login %s não está online.\nO uso do comando \'/msg\' é: \'/msg login_destino sua_mensagem\''%loginDestino
-			con.send(m.encode('utf-8'))
-	else:
-		con.send('Argumentos insuficientes para o comando \'/msg\'\nO uso do comando é: \'/msg login_destino sua_mensagem\''.encode('utf-8'))
-
 def agora(): # Retorna a data e hora atuais.
 	now = datetime.now()
-	return '{0:0=2d}:{1:0=2d} {2:0=2d}/{3:0=2d}/{4}'.format(now.hour, now.minute, now.day, now.month, now.year)
+	return '{0:0=2d}/{1:0=2d}/{2} {3:0=2d}:{4:0=2d}'.format(now.day, now.month, now.year, now.hour, now.minute)
 
 def getToken(autenticacao): # Retorna o token para acesso ao SUAP.
 	response = requests.post(urls['token'], data=autenticacao)
 	if response.status_code == 200:
 		return json.loads(response.content.decode('utf-8'))['token']
-	return 0
+	return None
 
 def getInformacoes(cabecalho): # Retorna os dados utilizando o token.
 	response = requests.get(urls['dados'], headers=cabecalho)
 	if response.status_code == 200:
 		return response.content.decode('utf-8')
-	return 0
+	return None
 
 def autentica(login,senha): # Define o login do cliente de acordo com o nome no SUAP.
 	autenticacao = { 'username': login, 'password': senha }
 	cabecalho = {'Authorization': 'JWT {0}'.format(getToken(autenticacao))}
-	informacoes = None
-	if cabecalho:
-		informacoes = json.loads(getInformacoes(cabecalho))
-	if informacoes:
-		login = informacoes['nome_usual'].replace(' ','_') # Ex.: nome_usual = 'Lucas Schneider'; login = 'Lucas_Schneider'
-		return login
-	return 0
+	informacoes = json.loads(getInformacoes(cabecalho))
+	login = informacoes['nome_usual'].replace(' ','_') # Ex.: nome_usual = 'Lucas Schneider'; login = 'Lucas_Schneider'
+	return login
 
 def setLogin(con, cliente):
+	msg = ''
 	while msg != 'OK':
 		login = con.recv(1024).decode('utf-8') # Receber login
 		senha = con.recv(1024).decode('utf-8') # Receber senha
-		
-		if (login == '\x00' or senha == '\x00'): msg = 'A matrícula e senha não podem ficar em branco. Tente novamente.'
-		else: login = autentica(login,senha) # Tentativa de login no SUAP
 
-		if login: msg = 'OK'
-		else: msg = 'Não foi possível realizar o login. Verifique se seu usuário e senha estão corretos.'
+		if (login == '\x00' or senha == '\x00'): msg = 'A matrícula e senha não podem ficar em branco. Tente novamente.'
+		else:
+			try:
+				login = autentica(login,senha) # Tentativa de login no SUAP
+				msg = 'OK'
+			except:
+				return 0
+
+		if msg != 'OK':
+			msg = 'Não foi possível realizar o login. Verifique se seu usuário e senha estão corretos.'
 
 		if login in logins.values(): # Se houver alguém online com aquele login
 			msg = 'Já há um usuário com esse login online. Tente novamente.'
@@ -167,8 +92,12 @@ def setLogin(con, cliente):
 		try: con.send(msg.encode('utf-8'))
 		except: return 0
 
+		if msg == 'OK': break
+
+	time.sleep(5)
 	try: con.send(login.encode('utf-8'))
 	except: return 0
+
 	logins[cliente] = login
 	conexoes[cliente] = con
 	horaInicio[cliente] = agora()
@@ -180,39 +109,48 @@ def inicioConexao(con, cliente):
 		msg = logins[cliente],' entrou.'
 		envioBroadcast(agora(),servername,msg)
 		OK_conexao = conexao(con,cliente)
+		if not OK_conexao: # Se a conexão foi encerrada abruptamente
+			msg = 'Algo deu errado ao conectar com o cliente ', cliente
+			logAdd(agora(),servername, msg)
 	else:
-		logAdd(agora(),servername, ('Algo deu errado ao conectar com o cliente ',cliente))
+		msg = 'Não foi possível autenticar o cliente ', cliente
+		logAdd(agora(),servername, msg)
 
-	if not OK_conexao: # Se a conexão foi encerrada abruptamente
-		del logins[cliente]
-		del conexoes[cliente]
-		del horaInicio[cliente]
+	
+
+	fimConexao(con, cliente)
 
 	con.close()
 	_thread.exit()
 
 def conexao(con,cliente):
-	while True:
-		msg = con.recv(1024).decode('utf-8')
-		if not msg or msg == '/exit': break
-		elif msg.split()[0] == '/msg': # Executa o comando /msg (envia mensagens privadas entre clientes).
-			envioPrivado(con, cliente, msg)
-		elif msg.split()[0] == '/clients': # Executa o comando /clients (envia para o cliente uma lista com os clientes ativos).
-			con.send(mostrarConexoes(1).encode('utf-8'))
-		else:
-			logAdd('{0} {1} {2}: {3}'.format(agora(), logins[cliente], cliente, msg))
-			msg = '{0} {1} diz: {2}'.format(agora(), logins[cliente], msg)
-			_thread.start_new_thread(envioBroadcast, tuple([msg, cliente]))
-	fimConexao(con, cliente)
+	while msg != 'exit': # 'exit' = Comando utilizado pelo cliente para encerrar a conexão
+		try:
+			msg = con.recv(1024).decode('utf-8')
+		except:
+			return 0
+		logAdd(agora(), logins[cliente], msg)
+		_thread.start_new_thread(envioBroadcast, tuple([agora(),logins[cliente],msg]))
+	return 1
 
 def fimConexao(con, cliente):
-	m = '{0} <servidor> {1} saiu.'.format(agora(), logins[cliente], cliente)
-	logAdd('{0} <servidor> {1} {2} saiu.'.format(agora(),logins[cliente], cliente))
-	del logins[cliente]
-	del conexoes[cliente]
-	del horaInicio[cliente]
-	mostrarConexoes(0)
-	_thread.start_new_thread(envioBroadcast, tuple([m, cliente]))
+	try:
+		msg = logins[cliente],' saiu.'
+		logAdd(agora(),logins[cliente], msg)
+	except:
+		pass
+
+	try: del logins[cliente]
+	except: pass
+
+	try: del conexoes[cliente]
+	except: pass
+
+	try: del horaInicio[cliente]
+	except: pass
+
+	try: _thread.start_new_thread(envioBroadcast, tuple([agora(),logins[cliente],msg]))
+	except: pass
 
 
 #MAIN
@@ -246,7 +184,8 @@ while True: # Loop para sempre aceitar novas conexões.
 		con, cliente = server.accept()
 	except KeyboardInterrupt:
 		break
-	logAdd('{0} <servidor> Nova thread iniciada para o cliente {1}'.format(agora(),cliente))
+	msg = 'Nova thread iniciada para o cliente ', cliente
+	logAdd(agora(),servername, msg)
 	_thread.start_new_thread(inicioConexao, tuple([con, cliente]))
 m = 'Encerrando servidor às %s...'%agora()
 mensagens.append(m)
